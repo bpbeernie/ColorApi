dd6587e1685177cbcc46c94e744fcdf3299f2a79
bpbeernie <bpbeernie@gmail.com>
2020-07-21 20:28:37 -0700

    added file

---
+++ Models/PrepDB.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace ColourAPI.Models
+{
+    public static class PrepDB
+    {
+    }
+}
